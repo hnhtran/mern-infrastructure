43d8edcbf89861c66e28fdf9be707ddd17738dd7
hnhtran <tran_eetas@yahoo.com>
2022-06-25 00:48:28 +1000

    MERN infrastructure pt2 complete

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,31 @@
-import './App.css';
+// css
+import "./App.css";
+// library
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+// routers
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <main className="App">
-      App
+      <NavBar />
+      {!user ? 
+        <AuthPage />
+       :
+       <> 
+       <Routes>
+          <Route path="/orders/new" element={<NewOrderPage />} />
+          <Route path='/orders' element={<OrderHistoryPage />} />
+        </Routes>
+       </> 
+        
+      }
     </main>
   );
 }
