5ce13edc4aaa748e45be42bd842e39ba686c5181
hnhtran <tran_eetas@yahoo.com>
2022-06-29 23:45:19 +1000

    mern infrastructure complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const usersCtrl = require('../../controllers/api/users')
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 // POST router /api/users
+// Create a new user
 router.post('/', usersCtrl.create)
+// login a user
 router.post('/login', usersCtrl.login)
+// check token
 router.get('/check-token', usersCtrl.checkToken)
 
 module.exports = router
